260f791cc44c738c5d45a4930ceea63a6a9faa41
M. Lima <marianalima.developer@gmail.com>
2025-05-05 18:35:54 +0100

    me estressei e apaguei a navbar, vou reescrever.

---
+++ StuddyBuddy/HomePage/Script.js
@@ -0,0 +1,12 @@
+const navbar = document.getElementById('navbar')
+
+function openSidebar()
+{
+ navbar.classList.add('show')
+}
+
+function closeSidebar()
+{
+    navbar.classList.remove('show')
+
+}
